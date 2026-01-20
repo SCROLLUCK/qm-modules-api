e6c3e7d8ba6eee34443be612e834dbb07d29b948
lucas.lima <lucas.lima@inovathus.org.br>
2026-01-20 17:33:51 -0400

    refactor: change api do socket.io server

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 import axios from "axios";
 import express from "express";
-const app = express();
-const port = 8000;
+import http from "http";
+import { Server } from "socket.io"; // Importa Server do Socket.IO
 
-app.get("/qm-modules-legacy", async (req, res) => {
-  const modules = [];
-  const response = await axios.get(
-    "https://api.github.com/users/modulesQuimera/repos?per_page=10000"
-  );
-  response.data.forEach((repo) => {
-    if (repo.name.startsWith("qm-") || repo.name.startsWith("qmb-")) {
-      modules.push(repo.name);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server); // Cria o servidor Socket.IO
+
+io.on("connection", (socket) => {
+  socket.on("message", async (msg) => {
+    // Escuta por eventos 'mensagem' do cliente
+    console.log("Mensagem recebida: " + msg);
+
+    if (msg === "GET MODULES") {
+      const modules = [];
+      const response = await axios.get(
+        "https://api.github.com/users/modulesQuimera/repos?per_page=10000",
+      );
+      response.data.forEach((repo) => {
+        if (repo.name.startsWith("qm-") || repo.name.startsWith("qmb-")) {
+          modules.push(repo.name);
+        }
+      });
+      io.emit("message", {
+        installer: modules,
+      }); // Envia a mensagem para todos os clientes
     }
   });
-  res.send({ modules: modules, total: modules.length });
+
+  socket.on("connect", (msg) => {
+    // Escuta por eventos 'mensagem' do cliente
+    console.log("Conexão estabelecida: " + msg);
+    io.emit("message", { msg: "Conexão estabelecida" }); // Envia a mensagem para todos os clientes
+  });
+
+  socket.on("disconnect", () => {
+    // Evento de desconexão
+    console.log("Usuário desconectado");
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+server.listen(3000, () => {
+  console.log("Servidor Socket.IO rodando na porta 3000");
 });
