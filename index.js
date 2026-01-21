d15449a9062b12f6a90b8bd904a4399f8a59d90e
lucas.lima <lucas.lima@inovathus.org.br>
2026-01-21 16:30:51 -0400

    feat: add default route to list modules

--- index.js
+++ index.js
@@ -40,6 +40,41 @@ io.on("connection", (socket) => {
   });
 });
 
+app.get("/", async (req, res) => {
+  try {
+    const modules = [];
+    const response = await axios.get(
+      "https://api.github.com/users/modulesQuimera/repos?per_page=10000",
+    );
+    response.data.forEach((repo) => {
+      if (repo.name.startsWith("qm-") || repo.name.startsWith("qmb-")) {
+        modules.push(repo.name);
+      }
+    });
+
+    res.send(`
+    <!doctype html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+      <h1>qm-modules-api</h1>
+        <ul>${modules
+          .map((module) => {
+            return `<li>${module}</li>`;
+          })
+          .join("")}
+        </ul>
+      </body>
+    </html>`);
+  } catch (e) {
+    res.send("Error on ");
+  }
+});
+
 server.listen(3000, () => {
   console.log("Servidor Socket.IO rodando na porta 3000");
 });
